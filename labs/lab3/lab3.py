096981ef6afcebe5c039c0fe73a33fd5117eeb41
Naaikiru <kiki01907@mail.ru>
2025-10-10 13:02:06 +1000

    kikikik

---
+++ labs/lab3/lab3.py
@@ -0,0 +1,58 @@
+# 1
+
+# n = input('your name: ')
+# a = input('your age: ')
+# print((f'меня зовут {n} и мне {a} лет. ') * 10)
+
+
+# 2
+
+# k = int(input('введите число от 1 до 9: '))
+# print('таблица умножения:')
+# print(f'{k} * 1 =', 1*k)
+# print(f'{k} * 2 =', 2*k)
+# print(f'{k} * 3 =', 3*k)
+# print(f'{k} * 4 =', 4*k)
+# print(f'{k} * 5 =', 5*k)
+# print(f'{k} * 6 =', 6*k)
+# print(f'{k} * 7 =', 7*k)
+# print(f'{k} * 8 =', 8*k)
+# print(f'{k} * 9 =', 9*k)
+# print(f'{k} * 10 =', 10*k)
+
+
+# 3
+
+# for i in range(0, 100, 3):
+#     print(i)
+
+
+# 4
+
+# n = int(input('введите число: '))
+# f = n
+# for i in range(2, n):
+#     f = f * i
+# print(f)
+
+
+# 5
+
+# n = 20
+# while n >= 0:
+#     print(n)
+#     n = n - 1
+
+
+# 6
+
+# n = int(input('введите число: '))
+# k = 0
+# l = 1
+# while k <= n:
+#     print(k, end= ' ')
+#     k, l = l, k + l
+
+
+# 7
+
