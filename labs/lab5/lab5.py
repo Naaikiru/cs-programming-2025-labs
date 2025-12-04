707b5cfba6bfb80442046039aadc193b0b7b434e
Naaikiru <kiki010907@mail.ru>
2025-12-04 21:47:44 +1000

    ijdaijfdsjfdshida

--- labs/lab5/lab5.py
+++ labs/lab5/lab5.py
@@ -112,3 +112,35 @@ def f(x, y):
     else: return('победитель бот!')
 
 print(f(igr1, rnd))
+
+
+# 9
+
+fr = ['сковорода', 'тумбочка', 'стакан', 'ноутбук', 'клавиатура', 'керамбит', 'артефакт']
+frend = {}
+
+for fr1 in fr:
+    frl = fr1[0]
+    if frl not in frend:
+        frend[frl] = []
+    frend[frl].append(fr1)
+print(frend)
+
+
+# 10
+
+st = [('Мария', [3, 4, 4]), ('Кюнней', [3, 5, 5]), ('Андрей', [4, 5, 4]), ('Айсен', [5, 5, 4]), ('Дарина', [2, 2, 3])]
+sl = {
+    st[0][0]: round(sum(st[0][1])/3, 1),
+    st[1][0]: round(sum(st[1][1])/3, 1),
+    st[2][0]: round(sum(st[2][1])/3, 1),
+    st[3][0]: round(sum(st[3][1])/3, 1),
+    st[4][0]: round(sum(st[4][1])/3, 1)
+}
+st2 = []
+st3 = []
+for st1 in sl.items():
+    st2.append(st1[1])
+    st3.append(st1[0])
+if max(st2):
+    print(f'{st3[st2.index(max(st2))]} имеет наивысший средний балл: {max(st2)}')
--- pp.py
+++ pp.py
@@ -1,19 +1,15 @@
-# 2
-
-a = int(input())
-n = int(input())
-
-def g(x, y):
-    r = (x //10000)*0.3 
-    pr = 0
-    pr1 = []
-    if n <= 3 : r += 3
-    if 4 <= n <=6 : r += 5
-    if n > 6 : r += 2
-    for i in range(1, y+1):
-        if (x+pr//10000) != r: 
-            r = ((x+pr)//10000)*0.3
-            pr = (x/100)*r
-            pr1.append(pr)
-    return pr1, sum(pr1)
-print(g(a, n))
+st = [('Мария', [3, 4, 4]), ('Кюнней', [3, 5, 5]), ('Андрей', [4, 5, 4]), ('Айсен', [5, 5, 4]), ('Дарина', [2, 2, 3])]
+sl = {
+    st[0][0]: round(sum(st[0][1])/3, 1),
+    st[1][0]: round(sum(st[1][1])/3, 1),
+    st[2][0]: round(sum(st[2][1])/3, 1),
+    st[3][0]: round(sum(st[3][1])/3, 1),
+    st[4][0]: round(sum(st[4][1])/3, 1)
+}
+st2 = []
+st3 = []
+for st1 in sl.items():
+    st2.append(st1[1])
+    st3.append(st1[0])
+if max(st2):
+    print(f'{st3[st2.index(max(st2))]} имеет наивысший средний балл: {max(st2)}')
