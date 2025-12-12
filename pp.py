5db27ad436373f81d9a7bafc1b5431cef935e893
Naaikiru <kiki01907@mail.ru>
2025-12-12 12:36:04 +1000

    пыавыавыа

--- pp.py
+++ pp.py
@@ -1,39 +1,46 @@
-# r = int(input())
+r = int(input())
 
-# print('1 матрица: ')
-# r1 =[]
-# for i in range(r):
-#     k = []
-#     h = input()
-#     k.append(h.split(' '))
-#     r1.append(k)
-#     # if  ValueError:
-#     #     print('no')
-#     #     print(quit())
-# print(len(r1[0]))
+print('1 матрица: ')
+r1 =[]
+for i in range(r):
+    h = input()
+    h = h.split(' ')
+    r1.append(h)
+for w in range(0, r):
+    if len(r1[w]) != r:
+        print('no')
+        print(quit())
 
-# if len(r1[0]) != r:
-#     print('no')
-#     print(quit())
+print('2 матрица: ')
+r2 =[]
+for i in range(r):
+    h = input()
+    h = h.split(' ')
+    r2.append(h)
+for w in range(0, r):
+    if len(r1[w]) != r:
+        print('no')
+        print(quit())
+d = []
+rr = int(r/2)
+for p in range(0, r):
+    for e in range(0, r):
+        d.append(int(r1[p][e]))
+d1 =[]
+for p in range(0, r):
+    for e in range(0, r):
+        d1.append(int(r2[p][e])) 
 
-# print('2 матрица: ')
-# r2 =[]
-# for i in range(r):
-#     k = []
-#     h = input()
-#     k.append(h.split(' '))
-#     r2.append(k)
-#     # if  ValueError:
-#     #     print('no')
-#     #     print(quit())
-# if len(r2[0]) != r:
-#     print('no')
-#     print(quit())
+n =[]
+n1 =[]
+for n2 in range(0, r):
+    for n3 in range(0, rr):
+        n.append(d[n3] + d1[n3])
+    for n3 in range(rr, r):
+        n1.append(d[n3] + d1[n3])
 
-# print(r1, r2)
-
-a = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-a = ', '.join(a)
-if '3' in a:
-    a = a.replace('3', '30', 1)
-    print(a)
+for i in range(0, r):
+    print('сумма этих двух матриц: ')
+    print(str(n[i]), str(n1[i]))   
+    print(str(n[i]), str(n1[i]))
+    break
