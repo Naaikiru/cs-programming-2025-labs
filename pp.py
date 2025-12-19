0c82265a5818dd155e2ce487624c73cb507e42fa
Naaikiru <kiki01907@mail.ru>
2025-12-19 12:55:36 +1000

    укенегншрго

--- pp.py
+++ pp.py
@@ -1,66 +1,43 @@
-# r = int(input())
+def add_matrices():
+    try:
+        n = int(input().strip())
+        
+        if n < 2:
+            print("Ошибка: размер матрицы должен быть больше 2")
+            return
+        
+        matrix1 = []
+        print(f"Введите {n} строк по {n} элементов для первой матрицы:")
+        for _ in range(n):
+            row = list(map(int, input().strip().split()))
+            if len(row) != n:
+                print(f"Ошибка: ожидается {n} элементов в строке")
+                return
+            matrix1.append(row)
+        
+        matrix2 = []
+        print(f"Введите {n} строк по {n} элементов для второй матрицы:")
+        for _ in range(n):
+            row = list(map(int, input().strip().split()))
+            if len(row) != n:
+                print(f"Ошибка: ожидается {n} элементов в строке")
+                return
+            matrix2.append(row)
+        
+        result_matrix = []
+        for i in range(n):
+            result_row = []
+            for j in range(n):
+                result_row.append(matrix1[i][j] + matrix2[i][j])
+            result_matrix.append(result_row)
+        
+        print('Сумма двух матриц:')
+        for row in result_matrix:
+            print(' '.join(map(str, row)))
+            
+    except ValueError:
+        print("Ошибка: некорректный ввод. Ожидается число для размера матрицы и числовые элементы.")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
 
-# print('1 матрица: ')
-# r1 =[]
-# for i in range(r):
-#     h = input()
-#     h = h.split(' ')
-#     r1.append(h)
-# for w in range(0, r):
-#     if len(r1[w]) != r:
-#         print('no')
-#         print(quit())
-
-# print('2 матрица: ')
-# r2 =[]
-# for i in range(r):
-#     h = input()
-#     h = h.split(' ')
-#     r2.append(h)
-# for w in range(0, r):
-#     if len(r1[w]) != r:
-#         print('no')
-#         print(quit())
-# d = []
-# rr = int(r/2)
-# for p in range(0, r):
-#     for e in range(0, r):
-#         d.append(int(r1[p][e]))
-# d1 =[]
-# for p in range(0, r):
-#     for e in range(0, r):
-#         d1.append(int(r2[p][e])) 
-
-# n =[]
-# n1 =[]
-# for n2 in range(0, r):
-#     for n3 in range(0, rr):
-#         n.append(d[n3] + d1[n3])
-#     for n3 in range(rr, r):
-#         n1.append(d[n3] + d1[n3])
-
-# for i in range(0, r):
-#     print('сумма этих двух матриц: ')
-#     print(str(n[i]), str(n1[i]))   
-#     print(str(n[i]), str(n1[i]))
-#     break
-
-def is_prime(n):
-    if n <= 1:
-         return False
-    for i in range(2, int(n**0.5) + 1):
-         if n % i == 0:
-            return False
-    return True
-
-h = input('введите начало и конец диапозона: ')
-h = h.split(' ')
-k = []
-h = [int(h[0]), int(h[1])]
-for i in range(h[0], h[1]+1):
-    if is_prime(i) == True:
-        k.append(i)
-if len(k) == 0:
-    print('Error!')
-else:
-    print(k)
+print(add_matrices())
