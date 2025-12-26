f46527101b4498baf4cddba6d2abca1f9ddb3a4f
Naaikiru <kiki01907@mail.ru>
2025-12-26 13:14:13 +1000

    dafafasdas

--- chmok.py
+++ chmok.py
@@ -1,8 +1,12 @@
-def poli():
-    p = input('введите строку на проверку "является ли она полидромом": ').lower()
-    p = p.replace(' ', '')
+vk = int(input('введите сумму, которую хотите вложить в банк: '))
+sr = int(input('введите срок (в годах): '))
 
-    if p == p[::-1]: return 'Да' 
-    else: return 'Нет' 
-
-print(poli())
+def obman(x, y):
+    r1 = (x // 10_000)/10
+    for r in range(1, y+1):
+        if r1 >= 5:
+            if sr <= 3:
+                r1 += 3
+                x += (x*r1/100)
+            return x
+print(obman(vk, sr))
