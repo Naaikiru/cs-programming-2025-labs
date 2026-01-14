4e7a2ff5f5e92331ca14ad4e5573366c2c6dcb22
Naaikiru <kiki010907@mail.ru>
2026-01-15 00:15:13 +1000

    ачонпроилио

--- chmok.py
+++ chmok.py
@@ -1,3 +1,29 @@
 vk = int(input('введите сумму, которую хотите вложить в банк: '))
 sr = int(input('введите срок (в годах): '))
 
+def vkl(x, y):
+    c = 0.3
+    rez = 0
+    if x < 30_000:
+        print('вклад должен быть не меньше 30 000 рублей')
+    
+    elif y == 3:
+        for i in range(y):
+            c = (c*(x//10_000) + 3)/100
+            if c >= 5:
+                c = 5/100
+                x = (((x + x*c)*c + (x + x*c))*c +((x + x*c)*c + (x + x*c))) - x
+                print(round(x, 2))
+            else:
+                x = (((x + x*c)*c + (x + x*c))*c +((x + x*c)*c + (x + x*c))) - x
+                print(round(x, 2))
+            break
+
+    elif 4 < y < 6:
+        for i in range(y):
+            c = (c*(x//10_000) + 5)/100
+            if c >= 5:
+                c = 5/100
+                            
+
+print(vkl(vk, sr))
--- pp.py
+++ pp.py
@@ -1,2 +1,24 @@
-g = 100 + (100*3.3/100)
-print(g)
+x = int(input('введите сумму, которую хотите вложить в банк: '))
+y = int(input('введите срок (в годах): '))
+def deposit_profit(a, n):
+    # увеличение от суммы (блоки по 10000 от 0)
+    blocks = a // 10000
+    increase = min(blocks * 0.003, 0.05)
+    
+    total = a
+    for year in range(1, n + 1):
+        if year <= 3:
+            base = 0.03
+        elif year <= 6:
+            base = 0.05
+        else:
+            base = 0.02
+        
+        rate = base + increase
+        total *= (1 + rate)
+    
+    profit = total - a
+    return round(profit, 2)
+
+# Проверка
+print(deposit_profit(x, y))  
